bf03a972fc84b22c6783d0c48c0fbd31a09e66c2
Mathieu Méa <mathieu.mea@gmail.com>
2015-06-19 10:01:39 -0400

    Improve French Canadian street types cleaning.

--- src/org/mtransit/parser/mt/data/MSpec.java
+++ src/org/mtransit/parser/mt/data/MSpec.java
@@ -413,12 +413,26 @@ public class MSpec {
 	// FR-CA : http://www.toponymie.gouv.qc.ca/ct/normes-procedures/terminologie-geographique/liste-termes-geographiques.html
 	private static final Pattern FR_CA_AVENUE = Pattern.compile(String.format(REGEX_START_END, "avenue"), Pattern.CASE_INSENSITIVE);
 	private static final String FR_CA_AVENUE_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Av.");
+	private static final Pattern FR_CA_AUTOROUTE = Pattern.compile(String.format(REGEX_START_END, "autoroute"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_AUTOROUTE_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Aut.");
+	private static final Pattern FR_CA_BOULEVARD = Pattern.compile(String.format(REGEX_START_END, "boulevard"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_BOULEVARD_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Boul.");
 	private static final Pattern FR_CA_CARREFOUR = Pattern.compile(String.format(REGEX_START_END, "carrefour"), Pattern.CASE_INSENSITIVE);
 	private static final String FR_CA_CARREFOUR_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Carref.");
+	private static final Pattern FR_CA_MONTAGNE = Pattern.compile(String.format(REGEX_START_END, "montagne"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_MONTAGNE_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Mgne");
+	private static final Pattern FR_CA_MONTEE = Pattern.compile(String.format(REGEX_START_END, "mont[é|e]e"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_MONTEE_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Mtée");
 	private static final Pattern FR_CA_PARC_INDUSTRIEL = Pattern.compile(String.format(REGEX_START_END, "parc industriel"), Pattern.CASE_INSENSITIVE);
 	private static final String FR_CA_PARC_INDUSTRIEL_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Parc Ind.");
-	private static final Pattern FR_CA_PLATEAU = Pattern.compile(String.format(REGEX_START_END, "plateau"), Pattern.CASE_INSENSITIVE);
-	private static final String FR_CA_PLATEAU_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Pl.");
+	private static final Pattern FR_CA_RIVIERE = Pattern.compile(String.format(REGEX_START_END, "rivi[e|è]re"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_RIVIERE_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Riv.");
+	private static final Pattern FR_CA_SECTEUR = Pattern.compile(String.format(REGEX_START_END, "secteur"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_SECTEUR_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Sect.");
+	private static final Pattern FR_CA_STATION_DE_METRO = Pattern.compile(String.format(REGEX_START_END, "Station de m[é|e]tro"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_STATION_DE_METRO_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Ston mét.");
+	private static final Pattern FR_CA_STATION = Pattern.compile(String.format(REGEX_START_END, "station"), Pattern.CASE_INSENSITIVE);
+	private static final String FR_CA_STATION_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Ston");
 	private static final Pattern FR_CA_TERRASSE = Pattern.compile(String.format(REGEX_START_END, "terrasse"), Pattern.CASE_INSENSITIVE);
 	private static final String FR_CA_TERRASSE_REPLACEMENT = String.format(REGEX_START_END_REPLACEMENT, "Tsse");
 	private static final Pattern FR_CA_TERRASSES = Pattern.compile(String.format(REGEX_START_END, "terrasses"), Pattern.CASE_INSENSITIVE);
@@ -426,9 +440,16 @@ public class MSpec {
 
 	public static String cleanStreetTypesFRCA(String string) {
 		string = FR_CA_AVENUE.matcher(string).replaceAll(FR_CA_AVENUE_REPLACEMENT);
+		string = FR_CA_AUTOROUTE.matcher(string).replaceAll(FR_CA_AUTOROUTE_REPLACEMENT);
+		string = FR_CA_BOULEVARD.matcher(string).replaceAll(FR_CA_BOULEVARD_REPLACEMENT);
 		string = FR_CA_CARREFOUR.matcher(string).replaceAll(FR_CA_CARREFOUR_REPLACEMENT);
+		string = FR_CA_MONTAGNE.matcher(string).replaceAll(FR_CA_MONTAGNE_REPLACEMENT);
+		string = FR_CA_MONTEE.matcher(string).replaceAll(FR_CA_MONTEE_REPLACEMENT);
 		string = FR_CA_PARC_INDUSTRIEL.matcher(string).replaceAll(FR_CA_PARC_INDUSTRIEL_REPLACEMENT);
-		string = FR_CA_PLATEAU.matcher(string).replaceAll(FR_CA_PLATEAU_REPLACEMENT);
+		string = FR_CA_RIVIERE.matcher(string).replaceAll(FR_CA_RIVIERE_REPLACEMENT);
+		string = FR_CA_SECTEUR.matcher(string).replaceAll(FR_CA_SECTEUR_REPLACEMENT);
+		string = FR_CA_STATION_DE_METRO.matcher(string).replaceAll(FR_CA_STATION_DE_METRO_REPLACEMENT);
+		string = FR_CA_STATION.matcher(string).replaceAll(FR_CA_STATION_REPLACEMENT);
 		string = FR_CA_TERRASSE.matcher(string).replaceAll(FR_CA_TERRASSE_REPLACEMENT);
 		string = FR_CA_TERRASSES.matcher(string).replaceAll(FR_CA_TERRASSES_REPLACEMENT);
 		return string;
