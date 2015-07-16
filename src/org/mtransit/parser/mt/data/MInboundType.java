b328fbafcdd90d973f2868d356be1dc39cc1d49f
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-16 15:41:02 -0400

    Fix Inbound/Outbound ID.

--- src/org/mtransit/parser/mt/data/MInboundType.java
+++ src/org/mtransit/parser/mt/data/MInboundType.java
@@ -2,7 +2,7 @@ package org.mtransit.parser.mt.data;
 
 public enum MInboundType {
 
-	NONE(""), INBOUND("0"), OUTBOUND("1");
+	NONE(""), INBOUND("1"), OUTBOUND("0");
 
 	public String id;
 
@@ -27,9 +27,9 @@ public enum MInboundType {
 
 	public int intValue() {
 		if (INBOUND.id.equals(this.id)) {
-			return 0;
-		} else if (OUTBOUND.id.equals(this.id)) {
 			return 1;
+		} else if (OUTBOUND.id.equals(this.id)) {
+			return 0;
 		} else {
 			System.out.printf("\nUnknow inbound type '%s'!\n", this.id);
 			System.exit(-1);
