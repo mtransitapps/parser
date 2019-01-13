48a858d43a21353048b52b0664c5e3ccd544bc59
Mathieu Méa <mathieu.mea@gmail.com>
2019-01-13 10:44:02 -0500

    Grand River Transit commons

---
+++ src/org/mtransit/commons/GrandRiverTransitCommons.java
@@ -0,0 +1,16 @@
+package org.mtransit.commons;
+
+public final class GrandRiverTransitCommons {
+
+	// TRIP DIRECTION ID USED BY REAL-TIME API
+
+	public static final int CLOCKWISE_SPLITTED_CIRCLE = 6000;
+	public static final int COUNTERCLOCKWISE_SPLITTED_CIRCLE = 9000;
+
+	public static final int EAST_SPLITTED_CIRCLE = 1000;
+	public static final int WEST_SPLITTED_CIRCLE = 2000;
+
+	public static final int NORTH_SPLITTED_CIRCLE = 3000;
+	public static final int SOUTH_SPLITTED_CIRCLE = 4000;
+
+}
