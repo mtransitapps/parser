722c7b4388494e8a4be57e716c3b22a729025262
Mathieu Méa <mathieu.mea@gmail.com>
2021-01-14 17:39:34 -0500

    DEV TOOLS: add regex scratch

---
+++ src/main/java/org/mtransit/scratch/RegexScratch.kt
@@ -0,0 +1,34 @@
+package org.mtransit.scratch
+
+import java.util.regex.Pattern
+
+@Suppress("JoinDeclarationAndAssignment", "CanBeVal")
+internal object RegexScratch {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var regex: String
+        regex = ""
+        println("regex: '$regex'.")
+        val pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE)
+
+        var string: String
+        string = ""
+        println("string: '$string'.")
+
+        val matcher = pattern.matcher(string)
+        while (matcher.find()) {
+            println("-------")
+            println("group: '" + matcher.group() + "'.")
+            println("groupCount: '" + matcher.groupCount() + "'.")
+            for (g in 0..matcher.groupCount()) {
+                println("group[" + g + "]: '" + matcher.group(g) + "'.")
+            }
+            println("-------")
+        }
+
+        var replaceAll: String
+        replaceAll = ""
+        println("replaceAll: '$replaceAll'.")
+        println("-> '" + pattern.matcher(string).replaceAll(replaceAll) + "'.")
+    }
+}
