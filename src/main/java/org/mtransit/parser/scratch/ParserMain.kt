f7bfe0f52168864bd401b25f0520c8d5dc0861bb
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-22 10:16:13 -0500

    add Main to quickly run/test code

---
+++ src/main/java/org/mtransit/parser/scratch/ParserMain.kt
@@ -0,0 +1,11 @@
+package org.mtransit.parser.scratch
+
+object ParserMain {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // QUICKLY RUN CODE
+        val result = "result"
+        println("result: '$result'.")
+    }
+}
