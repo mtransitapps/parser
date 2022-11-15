c0ea831d4a926b3d843b7c69a87502ace3981d8a
Mathieu Méa <mathieu.mea@gmail.com>
2022-11-15 14:25:21 -0500

    HTML symbols > hospital (WIP / feature flag)

--- src/main/java/org/mtransit/commons/HtmlSymbols.kt
+++ src/main/java/org/mtransit/commons/HtmlSymbols.kt
@@ -6,12 +6,20 @@ import org.mtransit.commons.Constants.SPACE_
 @Suppress("unused")
 object HtmlSymbols {
 
-    private const val DOWNWARDS_ARROW = "&#8595;" // https://unicode-table.com/en/2193/
-    private const val METRO = "&#128647;" // https://unicode-table.com/en/1F687/
+    private const val C_DOOR = "&#128682;" // https://unicode-table.com/en/1F6AA/
+    private const val C_DOWNWARDS_ARROW = "&#8595;" // https://unicode-table.com/en/2193/
+    private const val C_HOSPITAL = "&#127973;" // https://unicode-table.com/en/1F3E5/
+    private const val C_METRO = "&#128647;" // https://unicode-table.com/en/1F687/
 
     @JvmField // TODO const
-    val SUBWAY = if (FeatureFlags.F_HTML_POI_NAME) METRO else EMPTY
+    val SUBWAY = if (FeatureFlags.F_HTML_POI_NAME) C_METRO else EMPTY
 
     @JvmField // TODO const
     val SUBWAY_ = if (FeatureFlags.F_HTML_POI_NAME) SUBWAY + SPACE_ else EMPTY
+
+    @JvmField // TODO const
+    val HOSPITAL = if (FeatureFlags.F_HTML_POI_NAME) C_HOSPITAL else EMPTY
+
+    @JvmField // TODO const
+    val HOSPITAL_ = if (FeatureFlags.F_HTML_POI_NAME) HOSPITAL + SPACE_ else EMPTY
 }
