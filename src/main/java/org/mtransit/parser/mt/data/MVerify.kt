4fab4e4d19835e5b14fe23cf00b6cc463756b010
Mathieu Méa <mathieu.mea@gmail.com>
2025-11-28 11:04:53 -0500

    PR comments

--- src/main/java/org/mtransit/parser/mt/data/MVerify.kt
+++ src/main/java/org/mtransit/parser/mt/data/MVerify.kt
@@ -7,37 +7,19 @@ object MVerify {
 
     @JvmStatic
     fun verify(@Suppress("unused") mSpec: MSpec, agencyTools: DefaultAgencyTools) {
-        agencyTools.serviceIdToCleanupServiceId
-            .takeIf { agencyTools.verifyServiceIdsUniqueness() }
-            ?.let { map ->
-                if (map.values.toSet().size != map.keys.size) {
-                    throw MTLog.Fatal("ERROR: service IDs are not unique! \n$map")
-                }
-                MTLog.logDebug("Service IDs uniqueness verified.")
+        listOf(
+            Triple("service IDs", agencyTools.serviceIdToCleanupServiceId, agencyTools.verifyServiceIdsUniqueness()),
+            Triple("trip IDs", agencyTools.tripIdToCleanupTripId, agencyTools.verifyTripIdsUniqueness()),
+            Triple("route IDs", agencyTools.routeIdToCleanupRouteId, agencyTools.verifyRouteIdsUniqueness()),
+            Triple("stop IDs", agencyTools.stopIdToCleanupStopId, agencyTools.verifyStopIdsUniqueness())
+        ).forEach { (idTypeName, idMap, verificationEnabled) ->
+            if (!verificationEnabled) return@forEach
+            val valueToKeys = idMap.entries.groupBy({ it.value }, { it.key })
+            val duplicates = valueToKeys.filter { it.value.size > 1 }
+            if (duplicates.isNotEmpty()) {
+                throw MTLog.Fatal("ERROR: $idTypeName are not unique! Duplicates: \n$duplicates")
             }
-        agencyTools.tripIdToCleanupTripId
-            .takeIf { agencyTools.verifyTripIdsUniqueness() }
-            ?.let { map ->
-                if (map.values.toSet().size != map.keys.size) {
-                    throw MTLog.Fatal("ERROR: trip IDs are not unique! \n$map")
-                }
-                MTLog.logDebug("Trip IDs uniqueness verified.")
-            }
-        agencyTools.routeIdToCleanupRouteId
-            .takeIf { agencyTools.verifyRouteIdsUniqueness() }
-            ?.let { map ->
-                if (map.values.toSet().size != map.keys.size) {
-                    throw MTLog.Fatal("ERROR: route IDs are not unique! \n$map")
-                }
-                MTLog.logDebug("Route IDs uniqueness verified.")
-            }
-        agencyTools.stopIdToCleanupStopId
-            .takeIf { agencyTools.verifyStopIdsUniqueness() }
-            ?.let { map ->
-                if (map.values.toSet().size != map.keys.size) {
-                    throw MTLog.Fatal("ERROR: stop IDs are not unique! \n$map")
-                }
-                MTLog.logDebug("Stop IDs uniqueness verified.")
-            }
+            MTLog.logDebug("$idTypeName uniqueness verified.")
+        }
     }
 }
