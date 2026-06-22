2734d332d7fff9bb90e0f4d7414fbd170aa8743c
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-22 19:47:28 -0400

    Minor (logs)

--- src/main/java/org/mtransit/parser/mt/MDirectionSplitter.kt
+++ src/main/java/org/mtransit/parser/mt/MDirectionSplitter.kt
@@ -252,14 +252,14 @@ object MDirectionSplitter {
             throw MTLog.Fatal(
                 "$routeId: Unresolved situation! \n" +
                         "- ?: Trips: '${GIDs.toStringPlus(gTripIdInt)}': \n" +
-                        "  -> Stops: ${GIDs.toStringPlus(gStopIdInts, limit = -1)} \n" +
+                        "   -> Stops: ${GIDs.toStringPlus(gStopIdInts, limit = -1)} \n" +
                         " - ${directionsCandidates.size} candidates: \n" +
                         "---------- \n" +
                         "- 0: Trips: ${GIDs.toStringPlus(directionsCandidates.getOrNull(0)?.tripIdInts, limit = -1)}: \n" +
-                        "  -> Stops: ${GIDs.toStringPlus(directionsCandidates.getOrNull(0)?.stopIdInts, limit = -1)} \n" +
+                        "   -> Stops: ${GIDs.toStringPlus(directionsCandidates.getOrNull(0)?.stopIdInts, limit = -1)} \n" +
                         "---------- \n" +
-                        "- 1 Trips: ${GIDs.toStringPlus(directionsCandidates.getOrNull(1)?.tripIdInts, limit = -1)}: \n" +
-                        "  -> Stops: ${GIDs.toStringPlus(directionsCandidates.getOrNull(1)?.stopIdInts, limit = -1)}: \n" +
+                        "- 1: Trips: ${GIDs.toStringPlus(directionsCandidates.getOrNull(1)?.tripIdInts, limit = -1)}: \n" +
+                        "   -> Stops: ${GIDs.toStringPlus(directionsCandidates.getOrNull(1)?.stopIdInts, limit = -1)} \n" +
                         "---------- \n" +
                         "!"
             )
